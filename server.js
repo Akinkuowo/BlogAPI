d8a4213392472b8dbe6b1b7e7cac9beda20f2768
Akinkuowo <akinseun1990@gmail.com>
2020-01-08 17:05:32 +0100

    updating server

--- server.js
+++ server.js
@@ -25,23 +25,49 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post('/login', (req,res) => {
+    db.select('email', 'password').from('users')
+    .then('where', '=', req.body.email)
+    .then(response => {
+		const isValid = bcrypt.compareSync(password, response[0].hash);
+		if(isValid){
+			return db.select('*').from('users')
+			.where('email', '=', email)
+			.then(user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('unable to get user'))
+		}else{
+				res.status(400).json('username and password is incorrect')
+		}
+	}).catch(err => res.status(400).json('username and password is incorrect'))
+})
 
 app.post('/signup', (req, res) => { 
     const { email, name, password } = req.body;
+    if(!name || !email || !password){
+		return res.status(400).json('Bad form submittion request')
+	}
+    const hash = bcrypt.hashSync(password);
     db('users')
     .returning('*')
     .insert({
         name: name,
         email: email,
+        password: hash,
         joined: new Date()
     }).then(response => {
         res.json(response)
+    }).catch(error => res.status(400).json({
+        email: ['email already taken']
     })
+    
+    )
     
 })
 
 app.get('/', (req, res) => {
-    res.send(db('users'));
+    res.json('connected to');
 })
 
 // app.get('/', (req, res) => { res.json(db.users) })
